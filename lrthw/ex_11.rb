0f5c00ebdf37f70861ba3c2acb594085d8aa2852
James Newman <jamesmnewman1@gmail.com>
2014-10-03 14:59:47 -0700

    11

---
+++ lrthw/ex_11.rb
@@ -0,0 +1,12 @@
+print 'How old are you? '
+age = gets.chomp
+print 'How tall are you? '
+height = gets.chomp
+print 'How much do you weigh? '
+weight = gets.chomp
+
+#puts 'So you are #{age} old, #{height} tall, and #{weight} heavy'
+
+#weird. So the above line, is exactly the same as line 12, the only difference is 
+# single vs double quotes. 
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
